2ffe21cce8d87bd630bd9d9bca5d214b09d75dda
Donizeti Marcolino <donizetimarcolino@hotmail.com>
2020-01-03 16:25:39 -0300

    admin produtos busca e paginação

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -8,22 +8,35 @@ use \Hcode\Mailer;
 
 class Product extends Model {
 
-	public static function listAll(){
+	public static function listAll()
+	{
+
 		$sql = new Sql();
+
 		return $sql->select("SELECT * FROM tb_products ORDER BY desproduct");
+
 	}
-	public static function checkList($list){
-		foreach ($list as &$row) {			
+
+	public static function checkList($list)
+	{
+
+		foreach ($list as &$row) {
+			
 			$p = new Product();
 			$p->setData($row);
 			$row = $p->getValues();
+
 		}
+
 		return $list;
+
 	}
-	
+
 	public function save(){
+
 		$sql = new Sql();
-		$results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight,:desurl)", array(
+
+		$results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight, :desurl)", array(
 			":idproduct"=>$this->getidproduct(),
 			":desproduct"=>$this->getdesproduct(),
 			":vlprice"=>$this->getvlprice(),
@@ -33,25 +46,37 @@ class Product extends Model {
 			":vlweight"=>$this->getvlweight(),
 			":desurl"=>$this->getdesurl()
 		));
+
 		$this->setData($results[0]);
-		
+
 	}
 
-	public function get($idproduct){
+	public function get($idproduct)
+	{
+
 		$sql = new Sql();
+
 		$results = $sql->select("SELECT * FROM tb_products WHERE idproduct = :idproduct", [
 			':idproduct'=>$idproduct
 		]);
+
 		$this->setData($results[0]);
+
 	}
-	public function delete(){
+
+	public function delete()
+	{
+
 		$sql = new Sql();
+
 		$sql->query("DELETE FROM tb_products WHERE idproduct = :idproduct", [
 			':idproduct'=>$this->getidproduct()
-		]);		
+		]);
+
 	}
-	
-	public function checkPhoto(){
+
+	public function checkPhoto()
+	{
 
 		if (file_exists(
 			$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
@@ -74,7 +99,8 @@ class Product extends Model {
 
 	}
 
-	public function getValues(){
+	public function getValues()
+	{
 
 		$this->checkPhoto();
 
@@ -84,7 +110,8 @@ class Product extends Model {
 
 	}
 
-	public function setPhoto($file){
+	public function setPhoto($file)
+	{
 
 		$extension = explode('.', $file['name']);
 		$extension = end($extension);
@@ -118,10 +145,14 @@ class Product extends Model {
 		imagedestroy($image);
 
 		$this->checkPhoto();
+
 	}
-	
-	public function getFromURL($desurl){
+
+	public function getFromURL($desurl)
+	{
+
 		$sql = new Sql();
+
 		$rows = $sql->select("SELECT * FROM tb_products WHERE desurl = :desurl LIMIT 1", [
 			':desurl'=>$desurl
 		]);
@@ -129,13 +160,72 @@ class Product extends Model {
 		$this->setData($rows[0]);
 
 	}
-	public function getCategories(){
+
+	public function getCategories()
+	{
+
 		$sql = new Sql();
+
 		return $sql->select("
 			SELECT * FROM tb_categories a INNER JOIN tb_productscategories b ON a.idcategory = b.idcategory WHERE b.idproduct = :idproduct
 		", [
+
 			':idproduct'=>$this->getidproduct()
 		]);
+
 	}
+
+	public static function getPage($page = 1, $itemsPerPage = 10)
+	{
+
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products 
+			ORDER BY desproduct
+			LIMIT $start, $itemsPerPage;
+		");
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+
+	}
+
+	public static function getPageSearch($search, $page = 1, $itemsPerPage = 10)
+	{
+
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products 
+			WHERE desproduct LIKE :search
+			ORDER BY desproduct
+			LIMIT $start, $itemsPerPage;
+		", [
+			':search'=>'%'.$search.'%'
+		]);
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+
+	}
+
 }
+
  ?>
