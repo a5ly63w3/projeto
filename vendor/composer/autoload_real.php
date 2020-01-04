934709bdc42ea9139164005dba8a16662ba36e44
Donizeti Marcolino <donizetimarcolino@hotmail.com>
2020-01-03 21:21:48 -0300

    instalação do guzzle

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInitb567dcd7fcf136de5e10135b639d5d58
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitb567dcd7fcf136de5e10135b639d5d58::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequireb567dcd7fcf136de5e10135b639d5d58($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequireb567dcd7fcf136de5e10135b639d5d58($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
